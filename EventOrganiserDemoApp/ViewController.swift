f23ea4a43f624ee698aaea40e3b78052c5f653f1
ravi Mishra <ravimishra1017.rm@gmail.com>
2020-10-04 16:13:01 +0530

    Initial Commit

---
+++ EventOrganiserDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventOrganiserDemoApp
+//
+//  Created by Ravi Mishra on 04/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
